607ecab15169ebdded4ddecd5705f822bc008f10
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2024-09-23 09:18:56 +0700

    Push API Video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -13,6 +13,59 @@ const getListVideo = async(req, res) => {
     }
 }
 
+const getType = async (req, res) => {
+    try {
+        let data = await model.video_type.findAll();
+        return res.status(200).json(data);
+    } catch(error) {
+        return res.status(500).json({message: "error"});
+    }
+}
+
+const getListVideoType = async (req, res) => {
+    try{
+        let {typeId} = req.params;
+        let data = await model.video.findAll({
+            where: {
+                type_id: typeId
+            }
+        })
+        if (data.length == 0) {
+            return res.status(400).json({message: "No data"})
+        }
+        return res.status(200).json(data);
+    } catch(error) {
+        return res.status(500).json({message: "error"});
+    }
+}
+
+const getVideoPage = async (req, res) => {
+    try{
+        let {page, size} = req.params;
+        page = parseInt(page, 10); // NaN: not a number
+        size = parseInt(size, 10);
+        if (isNaN(page) || page <= 0) {
+            return res.status(400).json({message: "page is wrong"});
+        }
+        if (isNaN(size) || size <= 0) {
+            return res.status(400).json({message: "Size is wrong"});
+        }
+        let index = (page - 1) * size;
+
+        let data = await model.video.findAll({
+            offset: index,
+            limit: size
+        })
+
+        return res.status(200).json(data);
+    } catch(error) {
+        return res.status(500).json({message: "error"});
+    }
+}
+
 export {
     getListVideo,
+    getType,
+    getListVideoType,
+    getVideoPage,
 }
--- src/routes/video.router.js
+++ src/routes/video.router.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { getListVideo } from '../controllers/video.controller.js';
+import { getListVideo, getType, getListVideoType, getVideoPage } from '../controllers/video.controller.js';
 
 const videoRoutes = express.Router();
 videoRoutes.get("/get-videos", getListVideo);
-
+videoRoutes.get("/get-type", getType);
+videoRoutes.get("/get-video-type-by-id/:typeId", getListVideoType);
+videoRoutes.get("/get-video-page/:page/:size", getVideoPage);
 export default videoRoutes;
