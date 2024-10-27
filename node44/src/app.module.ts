0f4604235b675380128353c71e5340c0db32e1bf
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2024-10-27 14:12:50 +0700

    Update code

--- node44/src/app.module.ts
+++ node44/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoModule } from './video/video.module';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [VideoModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    VideoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- node44/src/main.ts
+++ node44/src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService)
 
   // add validatation input
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(8080);
+  const configSwagger = new DocumentBuilder()
+  .setTitle("Test API Youtube")
+  .setDescription("Danh sách các API về youtube mini")
+  .setVersion("1.0")
+  .build()
+
+  const swagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup("swagger", app, swagger);
+  const port = configService.get<number>('PORT')
+  await app.listen(port);
 }
 bootstrap();
